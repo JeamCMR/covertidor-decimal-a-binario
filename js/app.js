7a9814f2b6a15d841b8b46d89285e63f6ea9a420
COPSTA <tutomx16@gmail.com>
2024-04-08 14:39:39 -0500

    añade funcion para validar entrada y convertir decimal a binario

--- js/app.js
+++ js/app.js
@@ -4,26 +4,55 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+/**Covierte el valor decimal en binario */
 
-/**verifica el valor en la entrada numérica */
+function decimalToBinary(input) {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
 
-const checkUserInput = ( ) => {
-    // null,undefined,0 son valores false
-    /*
-    La funcion isNaN() toma una cadena o numero como parametro y evalua si es un NaN y devuelve True o false
-    */
-    if(!numberInput.value || isNaN(parseInt(numberInput.value))){ 
-        window.alert("Ingrese un valor decimal")
+    if (input == 0) {
+        result.innerText = "0";
+        return
     }
-    // console.log(numberInput.value);
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2; //Los retos de la division vienen siendo la conversion al numero decimal
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+
+    input = quotient; //Variable de control
+  }
+  console.log ("Inputs: ",  inputs);
+  console.log("Quotients: ",quotients);
+  console.log("Remainders: ",remainders);
+  result.innerText = remainders.reverse().join("");
 }
 
+/**valida el valor en la entrada numérica */
+
+const checkUserInput = () => {
+  // null,undefined,0 son valores false
+  /*
+    La funcion isNaN() toma una cadena o numero como parametro y evalua si es un NaN y devuelve True o false
+    */
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    window.alert("Ingrese un valor decimal");
+    result.innerText= "";
+    return;
+  }
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
 
 /**EVENTOS */
 convertBtn.addEventListener("click", checkUserInput);
 
-numberInput.addEventListener("keydown", (e)=>{
-    if (e.key == "Enter") {
-        checkUserInput();
-    }
-})
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    checkUserInput();
+  }
+});
