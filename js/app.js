992ccb1aac8d9231859a5be5d0d1df88984df22e
COPSTA <tutomx16@gmail.com>
2024-04-09 12:18:43 -0500

    añade funcion de animacion, explicaicon de pilas

--- js/app.js
+++ js/app.js
@@ -44,10 +44,29 @@ const result = document.getElementById("result");
 // }
 // countDownAndUp(3);
 
+const animationContainer = document.getElementById("animation-container");
+
+const animationData = [
+  {
+    inputVal:5, //valor de entrada cada vez que se ejecuta su funcion recursiva
+    marginTop:300, //margen superior para los elementos DOM que se agregan
+    addElDelay:1000
+  },
+  {
+    inputVal: -2,
+    marginTop: 200,
+    addElDelay: 1500
+  },
+  {
+    inputVal: 1,
+    marginTop: -200,
+    addElDelay: 2000
+  }
+];
+
 /**Covierte el valor decimal en binario */
 
 const decimalToBinary = (input) =>{
-  debugger
   if (input === 0 || input === 1){ //caso base donde se sale de la funcion
     return String(input);
   }
@@ -59,13 +78,28 @@ const decimalToBinary = (input) =>{
 }
 
 
+const showAnimation = () =>{
+  result.innerText = "Call Stack Animation";
+  animationData.forEach( (obj) => {
+    setTimeout(() => {
+      animationContainer.innerHTML += ``;
+    }, obj.addElDelay);
+  });
+}
+
 const checkUserInput = () => {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+const inputInt  = parseInt(numberInput.value);
+  if (!numberInput.value || isNaN(inputInt)) {
     alert("Please provide a decimal number");
     return;
   }
-  debugger
-  result.textContent = decimalToBinary(parseInt(numberInput.value));
+
+  if(inputInt === 5){
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
 };
 
